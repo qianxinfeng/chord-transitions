5e9187bf6e874d507fc3bf32814a3390cc6887c7
xinfeng_qian <xinfeng_qian@hansight.com>
2016-09-26 21:11:05 +0800

    添加构建

--- dist/d3-chord.js
+++ dist/d3-chord.js
@@ -1,149 +1,99 @@
+'use strict';
+
 angular.module('d3-chord', []).directive('d3Chord', ['$window', 'matrixFactory', function ($window, matrixFactory) {
-    let colorPalette=[
-        "#52c3f1", "#f77b6b", "#e3d64a", "#6767da", "#68d4b2", "#369d97",
-        "#74d9ed", "#f8a13f", "#dae342", "#8d7be3", "#a4e59b", "#54becc"
-    ];
-    var link = function ($scope, $el, $attr) {
-		$scope.id = "d3-" + $scope.$id;
-		let $container = $el, $tooltip = $container.find(".tooltip");
-		let marg = [50, 50, 50, 50]; // TOP, RIGHT, BOTTOM, LEFT
-		let size = getSize();
-        let colors = d3.scale.ordinal()
-            .range(colorPalette);
+    var colorPalette = ["#52c3f1", "#f77b6b", "#e3d64a", "#6767da", "#68d4b2", "#369d97", "#74d9ed", "#f8a13f", "#dae342", "#8d7be3", "#a4e59b", "#54becc"];
+    var link = function link($scope, $el, $attr) {
+        $scope.id = "d3-" + $scope.$id;
+        var $container = $el,
+            $tooltip = $container.find(".tooltip");
+        var marg = [50, 50, 50, 50]; // TOP, RIGHT, BOTTOM, LEFT
+        var size = getSize();
+        var colors = d3.scale.ordinal().range(colorPalette);
 
-        let chord = d3.layout.chord()
-            .padding(0.02)
-            .sortGroups(d3.descending)
-            .sortSubgroups(d3.ascending);
-        let matrix = matrixFactory.chordMatrix()
-            .layout(chord)
-            .filter(function (item, r, c) {
-                return (item.node1 === r.name && item.node2 === c.name) ||
-                    (item.node1 === c.name && item.node2 === r.name);
-            })
-            .reduce(function (items, r, c) {
-                var value;
-                if (!items[0]) {
-                    value = 0;
-                } else {
-                    value = items.reduce(function (m, n) {
-                        if (r === c) {
-                            return m + (n.weight1 + n.weight2);
-                        } else {
-                            return m + (n.node1 === r.name ? n.weight1 : n.weight2);
-                        }
-                    }, 0);
-                }
-                return {value: value, data: items};
-            });
+        var chord = d3.layout.chord().padding(0.02).sortGroups(d3.descending).sortSubgroups(d3.ascending);
+        var matrix = matrixFactory.chordMatrix().layout(chord).filter(function (item, r, c) {
+            return item.node1 === r.name && item.node2 === c.name || item.node1 === c.name && item.node2 === r.name;
+        }).reduce(function (items, r, c) {
+            var value;
+            if (!items[0]) {
+                value = 0;
+            } else {
+                value = items.reduce(function (m, n) {
+                    if (r === c) {
+                        return m + (n.weight1 + n.weight2);
+                    } else {
+                        return m + (n.node1 === r.name ? n.weight1 : n.weight2);
+                    }
+                }, 0);
+            }
+            return { value: value, data: items };
+        });
 
-        let innerRadius = (size.dims[1] / 2) - 100;
+        var innerRadius = size.dims[1] / 2 - 100;
 
-        let arc = d3.svg.arc()
-            .innerRadius(innerRadius)
-            .outerRadius(innerRadius + 20);
+        var arc = d3.svg.arc().innerRadius(innerRadius).outerRadius(innerRadius + 20);
 
-        let path = d3.svg.chord()
-            .radius(innerRadius);
+        var path = d3.svg.chord().radius(innerRadius);
 
+        var svg = d3.select($container[0]).append("svg").attr("class", "chart").attr({ width: size.width + "px", height: size.height + "px" }).attr("preserveAspectRatio", "xMinYMin").attr("viewBox", "0 0 " + size.width + " " + size.height);
 
-        let svg = d3.select($container[0]).append("svg")
-            .attr("class", "chart")
-            .attr({width: size.width + "px", height: size.height + "px"})
-            .attr("preserveAspectRatio", "xMinYMin")
-            .attr("viewBox", "0 0 " + size.width + " " + size.height);
-
-        let gContainer = svg.append("g")
-            .attr("class", "g-container")
-            .attr("transform", "translate(" + ((size.dims[0] / 2) + marg[3]) + "," + ((size.dims[1] / 2) + marg[0]) + ")");
-        let messages = svg.append("text")
-            .attr("class", "messages")
-            .attr("transform", "translate(10, 10)")
-            .text("Updating...");
+        var gContainer = svg.append("g").attr("class", "g-container").attr("transform", "translate(" + (size.dims[0] / 2 + marg[3]) + "," + (size.dims[1] / 2 + marg[0]) + ")");
+        var messages = svg.append("text").attr("class", "messages").attr("transform", "translate(10, 10)").text("Updating...");
 
         //绘制图形
         function draw(data) {
             messages.attr("opacity", 1);
             messages.transition().duration(1000).attr("opacity", 0);
-            matrix.data(data)
-                .resetKeys()
-                .addKeys(['node1', 'node2'])
-                .update();
-            let groups = gContainer.selectAll("g.group")
-                .data(matrix.groups(), function (d) {
-                    return d._id;
-                });
+            matrix.data(data).resetKeys().addKeys(['node1', 'node2']).update();
+            var groups = gContainer.selectAll("g.group").data(matrix.groups(), function (d) {
+                return d._id;
+            });
 
-            let gEnter = groups.enter()
-                .append("g")
-                .attr("class", "group");
+            var gEnter = groups.enter().append("g").attr("class", "group");
 
-            gEnter.append("path")
-                .on("click", groupClick)
-                .on("mouseover", dimChords)
-                .on("mouseout", resetChords)
-                .style("fill", function (d) {
-                    return colors(d._id);
-                })
-                .attr("d", arc);
+            gEnter.append("path").on("click", groupClick).on("mouseover", dimChords).on("mouseout", resetChords).style("fill", function (d) {
+                return colors(d._id);
+            }).attr("d", arc);
 
-            gEnter.append("text")
-                .style("pointer-events", "none")
-                .style("font-size", "9px")
-                .attr("dy", ".35em")
-                .text(function (d) {
-                    return d._id;
-                });
+            gEnter.append("text").style("pointer-events", "none").style("font-size", "9px").attr("dy", ".35em").text(function (d) {
+                return d._id;
+            });
 
-            groups.select("path")
-                .transition().duration(2000)
-                .attrTween("d", matrix.groupTween(arc));
+            groups.select("path").transition().duration(2000).attrTween("d", matrix.groupTween(arc));
 
-            groups.select("text")
-                .transition()
-                .duration(2000)
-                .attr("transform", function (d) {
-                    d.angle = (d.startAngle + d.endAngle) / 2;
-                    var r = "rotate(" + (d.angle * 180 / Math.PI - 90) + ")";
-                    var t = " translate(" + (innerRadius + 26) + ")";
-                    return r + t + (d.angle > Math.PI ? " rotate(180)" : " rotate(0)");
-                })
-                .attr("text-anchor", function (d) {
-                    return d.angle > Math.PI ? "end" : "begin";
-                });
+            groups.select("text").transition().duration(2000).attr("transform", function (d) {
+                d.angle = (d.startAngle + d.endAngle) / 2;
+                var r = "rotate(" + (d.angle * 180 / Math.PI - 90) + ")";
+                var t = " translate(" + (innerRadius + 26) + ")";
+                return r + t + (d.angle > Math.PI ? " rotate(180)" : " rotate(0)");
+            }).attr("text-anchor", function (d) {
+                return d.angle > Math.PI ? "end" : "begin";
+            });
 
             groups.exit().select("text").attr("fill", "orange");
             groups.exit().select("path").remove();
 
-            groups.exit().transition().duration(1000)
-                .style("opacity", 0).remove();
+            groups.exit().transition().duration(1000).style("opacity", 0).remove();
 
-            let chords = gContainer.selectAll("path.chord")
-                .data(matrix.chords(), function (d) {
-                    return d._id;
-                });
+            var chords = gContainer.selectAll("path.chord").data(matrix.chords(), function (d) {
+                return d._id;
+            });
 
-            chords.enter().append("path")
-                .attr("class", "chord")
-                .style("fill", function (d) {
-                    return colors(d.source._id);
-                })
-                .attr("d", path)
-                .on("mouseover", chordMouseover)
-                .on("mouseout", hideTooltip);
+            chords.enter().append("path").attr("class", "chord").style("fill", function (d) {
+                return colors(d.source._id);
+            }).attr("d", path).on("mouseover", chordMouseover).on("mouseout", hideTooltip);
 
-            chords.transition().duration(2000)
-                .attrTween("d", matrix.chordTween(path));
+            chords.transition().duration(2000).attrTween("d", matrix.chordTween(path));
 
             chords.exit().remove();
             //点击事件
             function groupClick(d) {
                 d3.event.preventDefault();
                 d3.event.stopPropagation();
-                let config = $scope.config;
+                var config = $scope.config;
                 if (config.callback && config.callback.groupClick) {
                     config.callback.groupClick(d);
-                    if(!$scope.$$phase && !$scope.$root.$$phase) $scope.$apply();
+                    if (!$scope.$$phase && !$scope.$root.$$phase) $scope.$apply();
                 }
                 resetChords();
             }
@@ -154,10 +104,10 @@ angular.module('d3-chord', []).directive('d3Chord', ['$window', 'matrixFactory',
                 d3.event.stopPropagation();
                 dimChords(d);
                 $tooltip.css("opacity", 1);
-                let config = $scope.config;
+                var config = $scope.config;
                 if (config.tooltip.formatter) {
-                    let info = config.tooltip.formatter(matrix.read(d));
-                    $tooltip.css({left:d3.event.offsetX+20,top:d3.event.offsetY+20}).html(info);
+                    var info = config.tooltip.formatter(matrix.read(d));
+                    $tooltip.css({ left: d3.event.offsetX + 20, top: d3.event.offsetY + 20 }).html(info);
                 }
             }
 
@@ -180,37 +130,41 @@ angular.module('d3-chord', []).directive('d3Chord', ['$window', 'matrixFactory',
                 d3.event.preventDefault();
                 d3.event.stopPropagation();
                 gContainer.selectAll("path.chord").style("opacity", function (p) {
-                    if (d.source) { // COMPARE CHORD IDS
-                        return (p._id === d._id) ? 0.9 : 0.1;
-                    } else { // COMPARE GROUP IDS
-                        return (p.source._id === d._id || p.target._id === d._id) ? 0.9 : 0.1;
+                    if (d.source) {
+                        // COMPARE CHORD IDS
+                        return p._id === d._id ? 0.9 : 0.1;
+                    } else {
+                        // COMPARE GROUP IDS
+                        return p.source._id === d._id || p.target._id === d._id ? 0.9 : 0.1;
                     }
                 });
             }
         }
 
         $scope.$watch("config.data", function (current, prev) {
-			draw(current);
+            draw(current);
         });
-		function getSize() {
-			let dims = []; // USABLE DIMENSIONS
-			let dimension = $scope.dimension;
-			let width, height,ratio=[1,1];
-			if(dimension){
-				ratio = dimension.split(":").reverse().map(Number);
-			}
-			let clientWidth = $container[0].clientWidth;
-			if(clientWidth<10){
-				width=1200;
-				height=800;
-			}else{
-				width = clientWidth;
-				height = width * ratio[0] / ratio[1];
-			}
-			dims[0] = width - marg[1] - marg[3]; // WIDTH
-			dims[1] = height - marg[0] - marg[2]; // HEIGHT
-			return {width,height,dims};
-		}
+        function getSize() {
+            var dims = []; // USABLE DIMENSIONS
+            var dimension = $scope.dimension;
+            var width = void 0,
+                height = void 0,
+                ratio = [1, 1];
+            if (dimension) {
+                ratio = dimension.split(":").reverse().map(Number);
+            }
+            var clientWidth = $container[0].clientWidth;
+            if (clientWidth < 10) {
+                width = 1200;
+                height = 800;
+            } else {
+                width = clientWidth;
+                height = width * ratio[0] / ratio[1];
+            }
+            dims[0] = width - marg[1] - marg[3]; // WIDTH
+            dims[1] = height - marg[0] - marg[2]; // HEIGHT
+            return { width: width, height: height, dims: dims };
+        }
         function resize() {
             size = getSize();
             svg.attr({
@@ -231,27 +185,32 @@ angular.module('d3-chord', []).directive('d3Chord', ['$window', 'matrixFactory',
         replace: true,
         scope: {
             config: "=",
-			dimension:"="
+            dimension: "="
         },
         link: link
     };
 }]).factory('matrixFactory', [function () {
 
-    var chordMatrix = function () {
+    var chordMatrix = function chordMatrix() {
 
-        var _matrix = [],objs=[],entry={}, dataStore = [], _id = 0;
-        var matrixIndex = [], indexHash = {};
+        var _matrix = [],
+            objs = [],
+            entry = {},
+            dataStore = [],
+            _id = 0;
+        var matrixIndex = [],
+            indexHash = {};
         var chordLayout, layoutCache;
 
-        var filter = function () {};
-        var reduce = function () {};
+        var filter = function filter() {};
+        var reduce = function reduce() {};
 
         var matrix = {};
 
         matrix.update = function () {
             _matrix = [], objs = [], entry = {};
 
-            layoutCache = {groups: {}, chords: {}};
+            layoutCache = { groups: {}, chords: {} };
 
             this.groups().forEach(function (group) {
                 layoutCache.groups[group._id] = {
@@ -286,7 +245,9 @@ angular.module('d3-chord', []).directive('d3Chord', ['$window', 'matrixFactory',
                         return filter(obj, indexHash[matrixIndex[i]], indexHash[matrixIndex[j]]);
                     });
                     entry = reduce(objs, indexHash[matrixIndex[i]], indexHash[matrixIndex[j]]);
-                    entry.valueOf = function () { return +this.value };
+                    entry.valueOf = function () {
+                        return +this.value;
+                    };
                     _matrix[i][j] = entry;
                 }
             }
@@ -332,14 +293,14 @@ angular.module('d3-chord', []).directive('d3Chord', ['$window', 'matrixFactory',
 
         matrix.addKey = function (key, data) {
             if (!indexHash[key]) {
-                indexHash[key] = {name: key, data: data || {}};
+                indexHash[key] = { name: key, data: data || {} };
             }
         };
 
         matrix.addKeys = function (props, fun) {
             for (var i = 0; i < dataStore.length; i++) {
                 for (var j = 0; j < props.length; j++) {
-                    this.addKey(dataStore[i][props[j]], fun ? fun(dataStore[i], props[j]):{});
+                    this.addKey(dataStore[i][props[j]], fun ? fun(dataStore[i], props[j]) : {});
                 }
             }
             return this;
@@ -353,7 +314,7 @@ angular.module('d3-chord', []).directive('d3Chord', ['$window', 'matrixFactory',
         function chordID(d) {
             var s = matrixIndex[d.source.index];
             var t = matrixIndex[d.target.index];
-            return (s < t) ? s + "__" + t: t + "__" + s;
+            return s < t ? s + "__" + t : t + "__" + s;
         }
 
         matrix.groupTween = function (d3_arc) {
@@ -365,8 +326,8 @@ angular.module('d3-chord', []).directive('d3Chord', ['$window', 'matrixFactory',
                     tween = d3.interpolateObject(cached, d);
                 } else {
                     tween = d3.interpolateObject({
-                        startAngle:d.startAngle,
-                        endAngle:d.startAngle
+                        startAngle: d.startAngle,
+                        endAngle: d.startAngle
                     }, d);
                 }
 
@@ -382,8 +343,8 @@ angular.module('d3-chord', []).directive('d3Chord', ['$window', 'matrixFactory',
                 var cached = layoutCache.chords[d._id];
 
                 if (cached) {
-                    if (d.source._id !== cached.source._id){
-                        cached = {source: cached.target, target: cached.source};
+                    if (d.source._id !== cached.source._id) {
+                        cached = { source: cached.target, target: cached.source };
                     }
                     tween = d3.interpolateObject(cached, d);
                 } else {
@@ -396,9 +357,9 @@ angular.module('d3-chord', []).directive('d3Chord', ['$window', 'matrixFactory',
                             }
                         }
                         if (groups.length > 0) {
-                            cached = {source: groups[0], target: groups[1] || groups[0]};
+                            cached = { source: groups[0], target: groups[1] || groups[0] };
                             if (d.source._id !== cached.source._id) {
-                                cached = {source: cached.target, target: cached.source};
+                                cached = { source: cached.target, target: cached.source };
                             }
                         } else {
                             cached = d;
@@ -426,25 +387,32 @@ angular.module('d3-chord', []).directive('d3Chord', ['$window', 'matrixFactory',
         };
 
         matrix.read = function (d) {
-            var g, m = {};
+            var g,
+                m = {};
 
             if (d.source) {
-                m.sname  = d.source._id;
-                m.sdata  = d.source.value;
+                m.sname = d.source._id;
+                m.sdata = d.source.value;
                 m.svalue = +d.source.value;
-                m.stotal = _matrix[d.source.index].reduce(function (k, n) { return k + n; }, 0);
-                m.tname  = d.target._id;
-                m.tdata  = d.target.value;
+                m.stotal = _matrix[d.source.index].reduce(function (k, n) {
+                    return k + n;
+                }, 0);
+                m.tname = d.target._id;
+                m.tdata = d.target.value;
                 m.tvalue = +d.target.value;
-                m.ttotal = _matrix[d.target.index].reduce(function (k, n) { return k + n; }, 0);
+                m.ttotal = _matrix[d.target.index].reduce(function (k, n) {
+                    return k + n;
+                }, 0);
             } else {
                 g = indexHash[d._id];
-                m.gname  = g.name;
-                m.gdata  = g.data;
+                m.gname = g.name;
+                m.gdata = g.data;
                 m.gvalue = d.value;
             }
             m.mtotal = _matrix.reduce(function (m1, n1) {
-                return m1 + n1.reduce(function (m2, n2) { return m2 + n2; }, 0);
+                return m1 + n1.reduce(function (m2, n2) {
+                    return m2 + n2;
+                }, 0);
             }, 0);
             return m;
         };
